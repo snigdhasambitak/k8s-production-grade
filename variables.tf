8d78c23a5b3215260e4451e8f97fbafc3776ac8b
Sambit Aryakumar <sambit@pop-messenger.io>
2019-06-17 17:10:10 +0530

    added the modules and variable files

---
+++ variables.tf
@@ -0,0 +1,49 @@
+# Variables Configuration
+
+variable "cluster-name" {
+  default     = "test-cluster"
+  type        = "string"
+  description = "The name of your EKS Cluster"
+}
+
+variable "aws-region" {
+  default     = "eu-west-1"
+  type        = "string"
+  description = "The AWS Region to deploy EKS"
+}
+
+variable "k8s-version" {
+  default     = "1.12"
+  type        = "string"
+  description = "Required K8s version"
+}
+
+variable "vpc-subnet-cidr" {
+  default     = "10.0.0.0/16"
+  type        = "string"
+  description = "The VPC Subnet CIDR"
+}
+
+variable "node-instance-type" {
+  default     = "m4.large"
+  type        = "string"
+  description = "Worker Node EC2 instance type"
+}
+
+variable "desired-capacity" {
+  default     = 2
+  type        = "string"
+  description = "Autoscaling Desired node capacity"
+}
+
+variable "max-size" {
+  default     = 5
+  type        = "string"
+  description = "Autoscaling maximum node capacity"
+}
+
+variable "min-size" {
+  default     = 1
+  type        = "string"
+  description = "Autoscaling Minimum node capacity"
+}
