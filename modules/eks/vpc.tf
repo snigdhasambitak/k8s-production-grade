d35c807e7fac57282b4151895f9cc10d752c252d
Sambit Aryakumar <8248474+snigdhasambitak@users.noreply.github.com>
2019-09-10 17:33:39 +0530

    Corrected the tags

--- modules/eks/vpc.tf
+++ modules/eks/vpc.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "eks" {
   count = 2
 
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-  cidr_block        = "${cidrsubnet(var.vpc-subnet-cidr, 8, count.index )}"
+  cidr_block        = "10.0.${count.index}.0/24"
   vpc_id            = "${aws_vpc.eks.id}"
 
   tags = "${
@@ -35,7 +35,7 @@ resource "aws_subnet" "eks" {
 resource "aws_internet_gateway" "eks" {
   vpc_id = "${aws_vpc.eks.id}"
 
-  tags {
+  tags = {
     Name = "${var.cluster-name}-eks-igw"
   }
 }
