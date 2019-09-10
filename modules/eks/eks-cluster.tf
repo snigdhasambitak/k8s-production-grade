33af5ea0d1e45b7f9f541ac44c73f07395f65181
Sambit Aryakumar <8248474+snigdhasambitak@users.noreply.github.com>
2019-09-10 17:31:23 +0530

    Updated some configs

--- modules/eks/eks-cluster.tf
+++ modules/eks/eks-cluster.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "cluster" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
+  tags = {
     Name = "${var.cluster-name}-eks-cluster-sg"
   }
 }
@@ -62,7 +62,7 @@ resource "aws_eks_cluster" "eks" {
 
   vpc_config {
     security_group_ids = ["${aws_security_group.cluster.id}"]
-    subnet_ids         = ["${aws_subnet.eks.*.id}"]
+    subnet_ids         = "${aws_subnet.eks[*].id}"
   }
 
   depends_on = [
